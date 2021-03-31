a4f490e1b4d10e16d903be6cec46c85e61a26294
MUHAMMAD TARIQ <81229484+TARIQ882@users.noreply.github.com>
2021-03-31 17:05:25 +0500

    Add files via upload

---
+++ String Function.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print("hello world")
+
+
+# In[9]:
+
+
+txt="my name is Muhammad Tariq"
+txt
+
+
+# # CAPITALIZE()
+
+# In[10]:
+
+
+txt.capitalize()
+
+
+# # UPPER()
+
+# In[11]:
+
+
+txt.upper()
+
+
+# # LOWER()
+
+# In[12]:
+
+
+txt.lower()
+
+
+# # IS UPPER()
+# 
+
+# In[13]:
+
+
+z=txt.upper()
+z.isupper()
+
+
+# # IS LOWER()
+
+# In[14]:
+
+
+r=txt.lower()
+r.islower()
+
+
+# # LENGHT()
+
+# In[15]:
+
+
+len(txt)
+
+
+# # CHECK STRING()
+# 
+
+# In[16]:
+
+
+st="i  live in mardan"
+print("live" in st)
+
+
+# # STRIP()
+
+# In[17]:
+
+
+q="     pakistan zindabad    "
+print(q.strip())
+
+
+# # R-STRIP()
+
+# In[18]:
+
+
+print(q.rstrip())
+
+
+# # L-STRIP()
+
+# In[19]:
+
+
+print(q.lstrip())
+
+
+# # find()
+
+# In[20]:
+
+
+print(q.find("a"))
+
+
+# # R-FIND()
+
+# In[21]:
+
+
+print(q.rfind("a"))
+
+
+# # COUNT()
+
+# In[22]:
+
+
+print(q.count("a"))
+
+
+# # REPLACE()
+
+# In[23]:
+
+
+print(q.replace("pakistan","python"))
+
+
+# # SPLIT()
+
+# In[24]:
+
+
+x="Sir Adil Mughal"
+print(x.split())
+
+
+# # ISALPHA()
+
+# In[26]:
+
+
+c="ASSIGNMENT"
+print(c.isalpha())
+
+
+# In[27]:
+
+
+print(x.isalpha())
+
+
+# # CENTER()
+
+# In[28]:
+
+
+v="I am doing BSCS"
+print(v.center(60))
+
+
+# # endswith()
+
+# In[29]:
+
+
+print(v.endswith("s"))
+
+
+# In[30]:
+
+
+print(v.endswith("S"))
+
+
+# # STARTSWITH()
+
+# In[31]:
+
+
+print(v.startswith("i"))
+
+
+# In[32]:
+
+
+print(v.startswith("I"))
+
+
+# # CASEFOLD()
+
+# In[33]:
+
+
+u="My favourite Hobby is cricket"
+print(u.casefold())
+
+
+# # SWAPCASE()
+
+# In[34]:
+
+
+y="my favourite colour is green"
+print(y.swapcase())
+
+
+# # ISALNUM()
+
+# In[35]:
+
+
+w="abcde12345"
+print(w.isalnum())
+
+
+# In[ ]:
+
+
+pr
+
